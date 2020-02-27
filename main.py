c86dc7f73d7429c77e84656fdaebaf46955a2457
Vikas Bhardwaj <vik.knit9140@gmail.com>
2020-02-27 13:20:13 +0530

    varable name changes and  sympell edited

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import chars2vec
 import sklearn.decomposition
 import matplotlib.pyplot as plt
 import pickle
+import operator
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -28,32 +29,36 @@ def load_pickel_emb(path):
 
 #finding the cosine_similarity ,
 
-def cosine_sim_word_index(vec1,vec2):
+def cosine_similar_word_index(vec1,vec2):
     # dist=cosine_similarity([vec1[0]],[vec2[0]])
-    cos_dist=[]
+    cosine_distance=[]
     for i in range(len(vec2)):
         dist=cosine_similarity([vec1[0]],[vec2[i]])
         
-        cos_dist.append((dist[0][0],vec2[i]))
-    # print(type(cos_dist))
-    cos_d=[]
-    for i in range(len(cos_dist)):
-        cos_d.append(cos_dist[i][0]) 
-    # print(cos_dist[0][0])
-    # max_cosine_val=max(cos_dist)[0]
+        cosine_distance.append((dist[0][0],vec2[i]))
 
-    max_cosine_val=max(cos_dist,key=lambda item:item[0])[0]
+    
+    # print(cos_d)
+
+    # max_cosine_val=max(cosine_distance,key=lambda item:item[0])[0]
+    # print(max_cosine_val)
+    sort_cosine_distance= sorted(cosine_distance,key = lambda i: i[0],reverse=True)[0:2]
+    # print(sort_cosine_distance)
     # print(max_cosine_val)
 
-    for i in range(len(cos_dist)):
-        if cos_dist[i][0] == max_cosine_val :
-            word_vec=cos_dist[i][1]
-    # print(max_cosine_val) 
+    # for i in range(len(cosine_distance)):
+    #     if cosine_distance[i][0] == max_cosine_val :
+    #         word_vec=cosine_distance[i][1]
+    # # print(max_cosine_val) 
     
     similar_word_index=[]
     for k in range(len(vec2)):
-        if (word_vec==vec2[k]).all():
-            similar_word_index.append(k)
+        for i in range(len(sort_cosine_distance)):
+            if (sort_cosine_distance[i][1]==vec2[k]).all():
+                if k not in similar_word_index:
+                    similar_word_index.append(k)
+    # print(similar_word_index)
+    # print(sort_cosine_distance)
             
     
     return similar_word_index
@@ -72,7 +77,7 @@ def cosine_sim_word_index(vec1,vec2):
 #             similar_word_index.append(k)
 # result=vec2[np.where(vec2==word_vec)]
 # print(result)
-# similar_word_index=cosine_sim_word_index(vec1,word_embeddings)
+# similar_word_index=cosine_similar_word_index(vec1,word_embeddings)
 # f = plt.figure(figsize=(8, 6))
 # # for j in range(len(projection_2d)):
 # plt.scatter(projection_2d[similar_word_index, 0], projection_2d[similar_word_index, 1],
@@ -88,12 +93,20 @@ if __name__ == "__main__":
     white_list_words = ['weight', 'unit', 'value', 'product', 'parties','partial', 'class', 'numeric', 'pieces/type', 'weight/volume', 'container' , 'count', 'hazmat', 'nmfc', 'ltl', 'serial', 'goods', 'package', 'weight/lbs', 'batch', 'item', 'product', 'ordered', 'tons', 'shipping', 'volume/weight', 'gross', 'marks',  'packs', 'goods', 'information', 'order', 'weight', 'rate', 'amount', 'charge', 'quantity', 'quantity', 'total', 'number of items', 'chargeable', 'price', 'pieces', 'kg', 'volume' , 'descending', 'description', 'descriptions', 'type','package', 'packaging', 'class', 'division','commodity', 'pallet', 'slip', 'group', 'value', 'gross', 'unit', 'ltl', 'nfmc', 'details',  'measurement','item details', 'item id', 'hem id', 'sale / lot', 'product','weight', 'unit', 'class', 'hash code', 'item', 'tariff', 'hazmat', 'commodity description', 'mark',  'description',  'serial number', 'part number', 'expiration date', 'lot number', 'item no', 'weight', 'number of pieces', 'no of pieces', 'rate charge', 'qty of goods', 'quantity of goods', 'quantity oft goods', 'nature and quantity', 'chargeable rate', 'rate class', 'po#', 'po #', 'item #', 'item#', 's no', 'description of', 'container no', 'piece count', 'po#', 'po #', 'no of pkgs', 'no of packages', 'volume/weight', 'volume / weight']
 
 
-    incorrect_word=["partis"]
+    incorrect_word=["weit"]
     word_embeddings_path="word_embeddings108.sav"
-    word_embeddings=load_pickel_emb(word_embeddings_path)
-    # print(word_embeddings.shape)
-    word_emb_incorrect_word=get_incorrect_word_emb(incorrect_word)
-    # print(word_emb)
-    similar_word_index=cosine_sim_word_index(word_emb_incorrect_word,word_embeddings)
-    # print(index)
-    print(white_list_words[similar_word_index[0]])
+    
+    def get_similar_words(incorrect_word,word_embeddings_path,white_list_words):
+        word_embeddings=load_pickel_emb(word_embeddings_path)
+        # print(word_embeddings.shape)
+        word_emb_incorrect_word=get_incorrect_word_emb(incorrect_word)
+        # print(word_emb)
+
+        similar_word_index=cosine_similar_word_index(word_emb_incorrect_word,word_embeddings)
+        # print(similar_word_index)
+        matched_words=[]
+        for i in range(len(similar_word_index)):
+            matched_words.append(white_list_words[i])
+            # print(matched_words)
+        return matched_words
+    print(get_similar_words(incorrect_word,word_embeddings_path,white_list_words))
--- symspell.py
+++ symspell.py
@@ -3,42 +3,48 @@
 import pkg_resources
 from symspellpy import SymSpell, Verbosity
 
-sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
-dictionary_path = pkg_resources.resource_filename(
-    "symspellpy", "frequency_dictionary_en_82_765.txt")
 
-# term_index is the column of the term and count_index is the
-# column of the term frequency
-sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
+def symspell_matched_word(incorrect_word):
+    incorrect_word=[incorrect_word]    
+    sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
+    dictionary_path = pkg_resources.resource_filename(
+        "symspellpy", "frequency_dictionary_en_82_765.txt")
 
-# lookup suggestions for single-word input strings
-# input_term = "Longuge"  # misspelling of "members"
+    # term_index is the column of the term and count_index is the
+    # column of the term frequency
+    sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
 
-# max edit distance per lookup
-# (max_edit_distance_lookup <= max_dictionary_edit_distance)
+    # lookup suggestions for single-word input strings
+    # input_term = "Longuge"  # misspelling of "members"
 
-words = ['Natural', 'Language', 'Understanding',
-         'Naturael', 'Longuge', 'Updderctundjing',
-         'Motural', 'Lamnguoge', 'Understaating',
-         'Naturrow', 'Laguage', 'Unddertandink',
-         'Nattural', 'Languagge', 'Umderstoneding']
-word=["expiration "]
-s=[]
-for  word in word:
-    
+    # max edit distance per lookup
+    # (max_edit_distance_lookup <= max_dictionary_edit_distance)
 
-    suggestions = sym_spell.lookup(word, Verbosity.CLOSEST,
-                                max_edit_distance=2,transfer_casing=True,ignore_token=r"\w+\d")
-    #keep the original casing
-    # Avoid correcting phrases matching regex
+    words = ['Natural', 'Language', 'Understanding',
+            'Naturael', 'Longuge', 'Updderctundjing',
+            'Motural', 'Lamnguoge', 'Understaating',
+            'Naturrow', 'Laguage', 'Unddertandink',
+            'Nattural', 'Languagge', 'Umderstoneding']
 
-    # display suggestion term, term frequency, and edit distance
-    for suggestion in suggestions:
-        # print(suggestion)
-        s.append((word,suggestion))
-for i in range(len(s)):
-    print(s[i][0],s[i][1])
-    # print(type(s[i][1]))
-    # for i in suggestion:
-        # print(i)\
-    
+    s=[]
+    # incorrect_word=["weght"]
+    for  word in incorrect_word:
+        
+
+        suggestions = sym_spell.lookup(word, Verbosity.CLOSEST,
+                                    max_edit_distance=2,transfer_casing=True,ignore_token=r"\w+\d")
+        #keep the original casing
+        # Avoid correcting phrases matching regex
+
+        # display suggestion term, term frequency, and edit distance
+        for suggestion in suggestions:
+            # print(suggestion)
+            s.append((word,suggestion))
+            # print(type(suggestion))
+
+    symspell_matched_words=[]
+    for i in range(len(s)):
+        # print(s[i][0],str(s[i][1]).split()[0][0:-1])
+        symspell_matched_words.append(str(s[i][1]).split()[0][0:-1])
+    return symspell_matched_words
+print(symspell_matched_word("weght"))    
