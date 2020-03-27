ec87f1c824724d9a5a39ef94d8de887203167e11
Vivek Prasad <vivek@koireader.com>
2020-03-27 21:54:58 +0530

    chore: numpy removed from setup.py

--- cython_utils/setup.py
+++ cython_utils/setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-import numpy
 
-setup(ext_modules=cythonize("*.pyx"), include_dirs=[numpy.get_include()])
+# import numpy
+
+setup(ext_modules=cythonize("*.pyx"))
+# setup(ext_modules=cythonize("*.pyx"), include_dirs=[numpy.get_include()])
